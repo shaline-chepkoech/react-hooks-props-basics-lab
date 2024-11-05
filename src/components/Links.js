9630052da8409f01fb9daae49704fed6b69b8176
shaline-chepkoech <chepkoechshaline726@gmail.com>
2024-11-05 21:25:14 +0300

    c1

---
+++ src/components/Links.js
@@ -0,0 +1,14 @@
+// src/components/Links.js
+import React from "react";
+
+function Links({ github, linkedin }) {
+  return (
+    <div>
+      <h3>Links</h3>
+      <a href={github} target="_blank" rel="noopener noreferrer">{github}</a>
+      <a href={linkedin} target="_blank" rel="noopener noreferrer">{linkedin}</a>
+    </div>
+  );
+}
+
+export default Links;
